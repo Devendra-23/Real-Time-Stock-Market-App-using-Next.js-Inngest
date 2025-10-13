ed53db4d19c1c50329f395eef452c4a3e5b6b351
Devendra-23 <dev6dhokare@gmail.com>
2025-10-13 20:31:47 +0530

    Auth done

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,16 @@
 "use client";
+import InputField from "@/components/forms/InputField";
+import SelectField from "@/components/forms/SelectField";
+import { CountrySelectField } from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
 import { Button } from "@/components/ui/button";
-import { date } from "better-auth";
+import { date, minLength } from "better-auth";
+
+import {
+  INVESTMENT_GOALS,
+  PREFERRED_INDUSTRIES,
+  RISK_TOLERANCE_OPTIONS,
+} from "@/lib/constants";
 import React from "react";
 import { useForm } from "react-hook-form";
 
@@ -34,15 +44,90 @@ const SignUp = () => {
     <>
       <h1 className="form-title">Sign Up & Personalise</h1>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="fullName"
+          label="Full Name"
+          placeholder="Thor Odinson"
+          register={register}
+          error={errors.fullName}
+          validation={{ required: "Full Name is required", minLength: 2 }}
+        />
+
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="Odinson@Asgard.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email name is required",
+            pattern: /^\w+@\w+\.\w+$/,
+            message: "Email address is required",
+          }}
+        />
+
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter your password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{ required: "Password is required", minLength: 8 }}
+        />
+
+        <CountrySelectField
+          name="country"
+          label="Country"
+          control={control}
+          error={errors.country}
+          required
+        />
+
+        <SelectField
+          name="investmentGoals"
+          label="Investment Goals"
+          placeholder="Select your investment goal"
+          options={INVESTMENT_GOALS}
+          control={control}
+          error={errors.investmentGoals}
+          required
+        />
+
+        <SelectField
+          name="riskTolerance"
+          label="Risk Tolerance"
+          placeholder="Select your risk level"
+          options={RISK_TOLERANCE_OPTIONS}
+          control={control}
+          error={errors.riskTolerance}
+          required
+        />
+
+        <SelectField
+          name="preferredIndustry"
+          label="Preferred Industry"
+          placeholder="Select your preferred industry"
+          options={PREFERRED_INDUSTRIES}
+          control={control}
+          error={errors.preferredIndustry}
+          required
+        />
+
         <Button
           type="submit"
           disabled={isSubmitting}
           className="yellow-btn w-full mt-5"
         >
-          {isSubmitting ? "Crating Account" : "Start Your Investing Journey"}
+          {isSubmitting ? "Creating Account" : "Start Your Investing Journey"}
         </Button>
+
+        <FooterLink
+          text="Don't have an account?"
+          linkText="Create an account"
+          href="/sign-up"
+        />
       </form>
-      ;
     </>
   );
 };
